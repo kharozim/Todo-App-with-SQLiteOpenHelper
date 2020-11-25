4dd3a102c27286ea3e309aac3d5f553ea6240808
kharozim <kharozim06@gmail.com>
2020-11-25 21:29:46 +0700

    setup api

--- app/src/main/java/com/kharozim/todo_app_with_sqliteopenhelper/repositories/remote/ApiClient.kt
+++ app/src/main/java/com/kharozim/todo_app_with_sqliteopenhelper/repositories/remote/ApiClient.kt
@@ -12,16 +12,23 @@ object ApiClient {
     const val BASE_URL = "https://online-course-todo.herokuapp.com/"
 
     private val gson: Gson by lazy { GsonBuilder().setLenient().create() }
-    private val interceptor : HttpLoggingInterceptor by lazy { HttpLoggingInterceptor().setLevel(
-        HttpLoggingInterceptor.Level.BODY) }
-    private val client : OkHttpClient by lazy { OkHttpClient.Builder().addInterceptor(interceptor).build() }
+    private val interceptor: HttpLoggingInterceptor by lazy {
+        HttpLoggingInterceptor().setLevel(
+            HttpLoggingInterceptor.Level.BODY
+        )
+    }
+    private val client: OkHttpClient by lazy {
+        OkHttpClient.Builder().addInterceptor(interceptor).build()
+    }
 
-    private val retrofit : Retrofit by lazy {
-        Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create(gson)).client(client).build()
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create(gson))
+            .client(client)
+            .build()
     }
 
 
-    val userService : TodoService by lazy {
+    val userService: TodoService by lazy {
         retrofit.create(TodoService::class.java)
     }
 
