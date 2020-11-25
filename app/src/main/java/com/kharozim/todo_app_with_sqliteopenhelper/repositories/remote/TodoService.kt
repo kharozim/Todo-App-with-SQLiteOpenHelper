870015af0a2b766c78ca864402d9ed58374926b1
Tommy Ferdian Hadimarta <trickytommy4@gmail.com>
2020-11-25 23:09:08 +0700

    add service

--- app/src/main/java/com/kharozim/todo_app_with_sqliteopenhelper/repositories/remote/TodoService.kt
+++ app/src/main/java/com/kharozim/todo_app_with_sqliteopenhelper/repositories/remote/TodoService.kt
@@ -1,9 +1,25 @@
 package com.kharozim.todo_app_with_sqliteopenhelper.repositories.remote
 
 import retrofit2.Call
-import retrofit2.http.GET
+import retrofit2.http.*
 
 interface TodoService {
     @GET("api/v1/todos")
-    fun getAllTodo() : Call<TodoResponse>
+    fun getAllTodo(): Call<TodoResponse>
+
+    @POST("api/v1/todos")
+    fun insertTodo(
+        @Query("task") task: String,
+        @Query("status") status: String
+    ): Call<TodoResponse>
+
+    @PUT("api/v1/todos/91")
+    fun putTodo(
+        @Query("task") task: String,
+        @Query("status") status: String
+    ): Call<TodoResponse>
+
+    @DELETE("api/v1/todos/91")
+    fun deleteTodo(@Path("id") id: Int): Call<TodoResponse>
+
 }
